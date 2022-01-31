9570fc93e1e1e529013c66a1b04baad94e0d3278
george-usynin-deriv <heorhi@deriv.com>
2022-01-31 19:51:40 +0300

    setup database connection

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
