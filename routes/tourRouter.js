2b63e272abbbaa6ff96ff9b17fd884fe024c880a
george-usynin-deriv <heorhi@deriv.com>
2022-02-10 13:13:03 +0300

    added aliasing for first 5 cheap tours

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,6 +6,8 @@ const router = express.Router();
 // We can add our own middleware for requested parameters (router.param)
 // router.param('id', tourController.checkID);
 
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
