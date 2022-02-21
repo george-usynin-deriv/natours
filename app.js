30a2908562ff85522bc1b0cc9046ea941a74c1c8
george-usynin-deriv <heorhi@deriv.com>
2022-02-21 15:36:17 +0300

    add new AppError class

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 // Logging routes in console
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -33,19 +34,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // Error handler middleware
 app.use((err, req, res, next) => {
+  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   res.status(err.statusCode).json({
