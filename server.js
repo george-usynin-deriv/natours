e731d5ac1db54c4541f82a852cb300c34fe883af
george-usynin-deriv <heorhi@deriv.com>
2022-02-23 15:42:24 +0300

    handle exceptions for synchronous code (global)

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// should be on the top of our code, handle exceptions for synchronous code
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,9 +28,10 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port} ...`);
 });
 
+// not mandatory to be on the top level, handle rejections of asynchronous code
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTON! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
