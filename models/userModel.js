e13b188ce9369ca352beceeb9a1ab2a59c7c6067
george-usynin-deriv <heorhi@deriv.com>
2022-02-23 19:38:33 +0300

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email address'],
+    validate: [validator.isEmail, 'Please provide a valid email address'],
+    unique: true,
+    lowercase: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must have at least 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
